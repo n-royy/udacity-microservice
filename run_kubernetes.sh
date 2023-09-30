129b5c10a4769fada3a2d0f447855e6ea1ef0125
n-royy <roynguyen18496@gmail.com>
2023-10-01 02:27:03 +0700

    Deploy with Kubernetes and Save Output Logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=n-royy/udacity-microservice
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-microservice --image $dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward udacity-microservice 8000:80
 
